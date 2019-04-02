e0d0a31579c08e728891cd7be72bd831fe098c35
zhangxiaoguang <517113739@qq.com>
2019-04-02 11:39:39 +0800

    Initial commit

---
+++ ARKitDemo/ARScnViewController.m
@@ -0,0 +1,36 @@
+//
+//  ARScnViewController.m
+//  ARKitDemo
+//
+//  Created by zhangxiaoguang on 2019/4/2.
+//  Copyright © 2019 zhangxiaoguang. All rights reserved.
+//
+
+#import "ARScnViewController.h"
+#import <ARKit/ARKit.h>
+#import <SceneKit/SceneKit.h>
+
+@interface ARScnViewController ()
+
+@property (nonatomic, strong) ARSCNView *scnView;
+
+@end
+
+@implementation ARScnViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ ARKitDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  ARKitDemo
+//
+//  Created by zhangxiaoguang on 2019/4/2.
+//  Copyright © 2019 zhangxiaoguang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
